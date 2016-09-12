19bca99ccf6130766c5317eb8891db38d51388eb
Shu Muto <shu-mutou@rf.jp.nec.com>
2016-09-12 15:51:42 +0900

    Setup python test environment
    This patch adds python test environment and test skeleton.
    Also need to set sphinx output to make the docs job pass
    at the same time.
    
    Change-Id: Icf5f2a210b7b02e90eb98dc285223ba4f7377a66
    Closes-Bug: #1608412

---
+++ zaqar_ui/test/api_tests/rest_api_tests.py
@@ -0,0 +1,48 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import mock
+
+from openstack_dashboard.test import helpers as test
+from openstack_dashboard.test.test_data.utils import TestData
+from zaqar_ui.api.rest import zaqar
+from zaqar_ui.test import test_data
+
+TEST = TestData(test_data.data)
+
+
+class ZaqarRestTestCase(test.TestCase):
+
+    # Queues
+    @mock.patch.object(zaqar, 'zaqar')
+    def test_queue_get(self, client):
+        # for check test env
+        self.assertTrue(1 * 1 == 1)
+
+    @mock.patch.object(zaqar, 'zaqar')
+    def test_queue_create(self, client):
+        # for check test env
+        self.assertTrue(1 + 1 == 2)
+
+    @mock.patch.object(zaqar, 'zaqar')
+    def test_queue_delete(self, client):
+        # for check test env
+        self.assertTrue(1 - 1 == 0)
+
+
+def mock_resource(resource):
+    """Utility function to make mocking more DRY"""
+
+    mocked_data = \
+        [mock.Mock(**{'to_dict.return_value': item}) for item in resource]
+
+    return mocked_data
---
+++ zaqar_ui/test/helpers.py
@@ -0,0 +1,39 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from openstack_dashboard.test import helpers
+from zaqar_ui import api
+from zaqar_ui.test import test_data
+from zaqarclient.queues.v2 import client as zaqar_client
+
+
+class APITestCase(helpers.APITestCase):
+    """Extends the base Horizon APITestCase for zaqarclient"""
+
+    def setUp(self):
+        super(APITestCase, self).setUp()
+        self._original_magnumclient = api.zaqar.zaqarclient
+        api.zaqar.zaqarclient = lambda request: self.stub_zaqarclient()
+
+    def _setup_test_data(self):
+        super(APITestCase, self)._setup_test_data()
+        test_data.data(self)
+
+    def tearDown(self):
+        super(APITestCase, self).tearDown()
+        api.zaqar.zaqarclient = self._original_zaqarclient
+
+    def stub_zaqarclient(self):
+        if not hasattr(self, "zaqarclient"):
+            self.mox.StubOutWithMock(zaqar_client, 'Client')
+            self.zaqarclient = self.mox.CreateMock(zaqar_client.Client)
+        return self.zaqarclient
