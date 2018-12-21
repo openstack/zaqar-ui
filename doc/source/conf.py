f54bf31732981b8780900af7437cf89febe2e7ed
“e <cuizc@inspur.com>
2018-12-21 15:14:13 +0800

    One more letter d
    Change-Id: I45b0dc4b8491b4330bbf27e43d8562ce76fac2d5

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,7 +13,7 @@
 # Horizon documentation build configuration file, created by
 # sphinx-quickstart on Thu Oct 27 11:38:59 2011.
 #
-# This file is execfile()d with the current directory set to its
+# This file is execfile() with the current directory set to its
 # containing dir.
 #
 # Note that not all possible configuration values are present in this
